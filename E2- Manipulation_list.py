c51b0df2cff399a138e9f5b353cffc3c455ccad8
Chanakya689 <chanakyamangaraju@gmail.com>
2026-06-18 11:47:41 +0530

    Add list manipulation operations including append, extend, insert, sort, map, filter, slicing, and copying

---
+++ E2- Manipulation_list.py
@@ -0,0 +1,27 @@
+#Manuplation list
+l = [1, 2, 3, 4, 5]
+l.append(6)
+print("List after appending 6:", l)
+
+l.extend([7, 8, 9])
+print("List after extending with [7, 8, 9]:", l)
+
+l.insert(0, 0)
+print("List after inserting 0 at index 0:", l) 
+
+l.sort()
+print("List after sorting:", l)
+
+print(list(map(lambda x: x*2, l)))
+print(list(filter(lambda x: x%2==0, l)))
+
+#slicing a list
+print("Slicing the list from index 2 to 5:", l[2:5])
+print("Slicing the list from index 0 to 3:", l[0:3])
+print("Slicing the list from index 3 to the end:", l[3:]) 
+print("Slicing the list from the start to index 4:", l[:4])  
+
+#copying a list
+l2 = l.copy()
+print("Copied list l2 :", l2)
+
