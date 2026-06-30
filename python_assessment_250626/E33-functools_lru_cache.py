6604e88c635d6aa794a41be6a3a6f9eb914c87c1
Chanakya689 <chanakyamangaraju@gmail.com>
2026-06-30 20:08:06 +0530

    fibonaicc lru_cache

---
+++ python_assessment_250626/E33-functools_lru_cache.py
@@ -0,0 +1,11 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def f(n):
+    if n<2:
+        return n
+    else:
+        return f(n-1)+f(n-2)
+    
+print(f(50))    
+print(f.cache_info())
